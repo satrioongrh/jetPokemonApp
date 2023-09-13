0530409ab90bf5239312c4008ef1d14f2f92d2f7
satrioongrh <ngrhsatrio9@gmail.com>
2023-09-13 14:54:05 +0700

    add detail info

--- app/src/main/java/com/example/jetpokedexapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/example/jetpokedexapp/ui/screen/detail/DetailScreen.kt
@@ -2,14 +2,22 @@ package com.example.jetpokedexapp.ui.screen.detail
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
@@ -18,21 +26,32 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.jetpokedexapp.R
 import com.example.jetpokedexapp.data.response.PokemonResponse
+import com.example.jetpokedexapp.data.response.TypesItem
 import com.example.jetpokedexapp.data.viewmodel.PokemonDetailViewModel
 import com.example.jetpokedexapp.util.Resource
+import com.example.jetpokedexapp.util.parseTypeToColor
+import java.lang.Math.round
+import java.util.Locale
 
 @Composable
 fun DetailScreen(
@@ -143,7 +162,11 @@ fun PokemonDetailStateWrapper(
 ) {
     when(pokemonInfo) {
         is Resource.Succes -> {
-
+            PokemonDetailSection(
+                pokemonInfo = pokemonInfo.data!!,
+                modifier = modifier
+                    .offset(y = (-20).dp)
+            )
         }
         is Resource.Error -> {
             Text(
@@ -158,5 +181,118 @@ fun PokemonDetailStateWrapper(
                 modifier = loadingModifier
             )
         }
+
+        else -> {}
+    }
+}
+
+@Composable
+fun PokemonDetailSection(
+    pokemonInfo: PokemonResponse,
+    modifier: Modifier = Modifier
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .offset(y = 100.dp)
+            .verticalScroll(scrollState)
+    ) {
+        Text(
+            text = "#${pokemonInfo.id} ${pokemonInfo.name?.capitalize(Locale.ROOT)}",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        PokemonTypeSection(types = pokemonInfo.types)
+        PokemonDetailDataSection(
+            pokemonWeight = pokemonInfo.weight!!,
+            pokemonHeight = pokemonInfo.height!!
+        )
+
+    }
+}
+
+@Composable
+fun PokemonTypeSection(types: List<TypesItem?>?) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+        for(type in types!!) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp)
+                    .clip(CircleShape)
+                    .background(parseTypeToColor(type))
+                    .height(35.dp)
+            ) {
+                Text(
+                    text = type?.type?.name?.capitalize(Locale.ROOT)!!,
+                    color = Color.White,
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PokemonDetailDataSection(
+    pokemonWeight: Int,
+    pokemonHeight: Int,
+    sectionHeight: Dp = 80.dp
+) {
+    val pokemonWeightInKg = remember {
+        round(pokemonWeight * 100f) / 1000f
+    }
+    val pokemonHeightInMeters = remember {
+        round(pokemonHeight * 100f) / 1000f
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        PokemonDetailDataItem(
+            dataValue = pokemonWeightInKg,
+            dataUnit = "kg",
+            dataIcon = painterResource(id = R.drawable.iw_weight),
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier
+            .size(1.dp, sectionHeight)
+            .background(Color.LightGray))
+        PokemonDetailDataItem(
+            dataValue = pokemonHeightInMeters,
+            dataUnit = "m",
+            dataIcon = painterResource(id = R.drawable.ic_height),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun PokemonDetailDataItem(
+    dataValue: Float,
+    dataUnit: String,
+    dataIcon: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Icon(painter = dataIcon, contentDescription = null, tint = MaterialTheme.colorScheme.onSurface)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "$dataValue$dataUnit",
+            color = MaterialTheme.colorScheme.onSurface
+        )
     }
 }
---
+++ app/src/main/java/com/example/jetpokedexapp/util/PokemonParse.kt
@@ -0,0 +1,78 @@
+package com.example.jetpokedexapp.util
+
+import androidx.compose.ui.graphics.Color
+import com.example.jetpokedexapp.data.response.Stat
+import com.example.jetpokedexapp.data.response.TypesItem
+import com.example.jetpokedexapp.ui.theme.AtkColor
+import com.example.jetpokedexapp.ui.theme.DefColor
+import com.example.jetpokedexapp.ui.theme.HPColor
+import com.example.jetpokedexapp.ui.theme.SpAtkColor
+import com.example.jetpokedexapp.ui.theme.SpDefColor
+import com.example.jetpokedexapp.ui.theme.SpdColor
+import com.example.jetpokedexapp.ui.theme.TypeBug
+import com.example.jetpokedexapp.ui.theme.TypeDark
+import com.example.jetpokedexapp.ui.theme.TypeDragon
+import com.example.jetpokedexapp.ui.theme.TypeElectric
+import com.example.jetpokedexapp.ui.theme.TypeFairy
+import com.example.jetpokedexapp.ui.theme.TypeFighting
+import com.example.jetpokedexapp.ui.theme.TypeFire
+import com.example.jetpokedexapp.ui.theme.TypeFlying
+import com.example.jetpokedexapp.ui.theme.TypeGhost
+import com.example.jetpokedexapp.ui.theme.TypeGrass
+import com.example.jetpokedexapp.ui.theme.TypeGround
+import com.example.jetpokedexapp.ui.theme.TypeIce
+import com.example.jetpokedexapp.ui.theme.TypeNormal
+import com.example.jetpokedexapp.ui.theme.TypePoison
+import com.example.jetpokedexapp.ui.theme.TypePsychic
+import com.example.jetpokedexapp.ui.theme.TypeRock
+import com.example.jetpokedexapp.ui.theme.TypeSteel
+import com.example.jetpokedexapp.ui.theme.TypeWater
+import java.util.Locale
+
+fun parseTypeToColor(type: TypesItem?): Color {
+    return when(type?.type?.name?.toLowerCase(Locale.ROOT)) {
+        "normal" -> TypeNormal
+        "fire" -> TypeFire
+        "water" -> TypeWater
+        "electric" -> TypeElectric
+        "grass" -> TypeGrass
+        "ice" -> TypeIce
+        "fighting" -> TypeFighting
+        "poison" -> TypePoison
+        "ground" -> TypeGround
+        "flying" -> TypeFlying
+        "psychic" -> TypePsychic
+        "bug" -> TypeBug
+        "rock" -> TypeRock
+        "ghost" -> TypeGhost
+        "dragon" -> TypeDragon
+        "dark" -> TypeDark
+        "steel" -> TypeSteel
+        "fairy" -> TypeFairy
+        else -> Color.Black
+    }
+}
+
+fun parseStatToColor(stat: Stat): Color {
+    return when(stat.name?.toLowerCase()) {
+        "hp" -> HPColor
+        "attack" -> AtkColor
+        "defense" -> DefColor
+        "special-attack" -> SpAtkColor
+        "special-defense" -> SpDefColor
+        "speed" -> SpdColor
+        else -> Color.White
+    }
+}
+
+fun parseStatToAbbr(stat: Stat): String {
+    return when(stat.name?.toLowerCase()) {
+        "hp" -> "HP"
+        "attack" -> "Atk"
+        "defense" -> "Def"
+        "special-attack" -> "SpAtk"
+        "special-defense" -> "SpDef"
+        "speed" -> "Spd"
+        else -> ""
+    }
+}
